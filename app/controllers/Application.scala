937150e908cb863738572a6029610894ab4b62e7
philm <phil.maccart@slalom.com>
2013-10-30 16:03:14 -0500

    complete tutoral

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,6 +2,9 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import models.Task
 
 object Application extends Controller {
   
@@ -9,10 +12,28 @@ object Application extends Controller {
     Redirect(routes.Application.tasks)
   }
 
-  def tasks = TODO
+  def tasks = Action {
+    Ok(views.html.index(Task.all(), taskForm))
+  }
 
-  def newTask = TODO
+  def newTask = Action { implicit request =>
+    taskForm.bindFromRequest.fold(
+      errors => BadRequest(views.html.index(Task.all(), errors)),
+      label => {
+        Task.create(label)
+        Redirect(routes.Application.tasks)
+      }
+    )
+  }
 
-  def deleteTask(id: Long) = TODO
+
+  def deleteTask(id: Long) = Action {
+    Task.delete(id)
+    Redirect(routes.Application.tasks)
+  }
+
+  val taskForm = Form(
+  "label" -> nonEmptyText
+  )
   
 }
